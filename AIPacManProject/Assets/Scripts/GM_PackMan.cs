2d8ef32209217a4b8c7825f6d786da2f2c8bea70
Oliver Chant <125320857+Deaf-Boy@users.noreply.github.com>
2023-03-31 01:48:42 +0100

    First Push
    A start to this hell

---
+++ AIPacManProject/Assets/Scripts/GM_PackMan.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GM_PackMan : MonoBehaviour
+{
+    public Text Score;
+    public Text Lives;
+
+    public int score { get; private set; }
+    public int lives { get; private set; }
+
+
+    void Start()
+    {
+        newGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Setscore(score + 1);
+        if (Input.GetKeyDown("space"))
+        {
+            Setlives(lives - 1);
+        }
+    }
+
+    private void newGame()
+    {
+        Setscore(0);
+        Setlives(3);
+    }
+
+    private void Setlives(int lives)
+    {
+        this.lives = lives;
+        Lives.text = "!" + Lives.ToString();
+    }
+
+    private void Setscore(int score)
+    {
+        this.score = score;
+        Score.text = score.ToString().PadLeft(2, '0'); 
+    }
+}
